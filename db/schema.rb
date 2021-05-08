c7e79433e19929fdf8875388f2ad26ca6792ce87
Vincenzo <vinzgameslol@gmail.com>
2021-05-08 19:30:22 +0200

    Working on devise installation problems.

--- db/schema.rb
+++ db/schema.rb
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(version: 2021_05_08_132647) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
